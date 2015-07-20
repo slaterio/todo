f5eaba0affc3e6153d8db8afa6c63b40a2bb8bba
Ben Slater <ben@overmation.com>
2015-07-20 17:39:03 +0100

    rewrote todo items controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,18 +3,18 @@ class TodoItemsController < ApplicationController
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
-
-		redirect_to @todo_list
+		redirect_to todo_list_path(@todo_list)
 	end
 
 	private
 
 	def set_todo_list
 		@todo_list = Todolist.find(params[:todo_list_id])
-	end	
+	end
 
 	def todo_item_params
 		params[:todo_item].permit(:content)
-	end
+	end	
+
 
 end
